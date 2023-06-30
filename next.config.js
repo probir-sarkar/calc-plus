0246e80485b813b7ce547d110f6ff8ecbdaff42c
Probir Sarkar <probirsarkar855@gmail.com>
2023-06-30 14:36:58 +0530

    test pwa

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   output: "export",
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA(nextConfig);
